418e0ca1f9ff786f90365503a61a846eb1246fd5
Cake <cakemancerr@gmail.com>
2022-09-01 16:56:29 +0200

    Add updater function on card setter

--- src/App.js
+++ src/App.js
@@ -1,65 +1,67 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import Header from './components/Header';
 import Content from './components/Content/Content';
 import Footer from './components/Footer';
-import {cardList} from './assets/cardList'
+import { cardList } from './assets/cardList';
 
 function App() {
-  let bestScore = 0;
-  const [score, setScore] = useState(0);
-  const [cards, setCards] = useState(cardList);
+	const [score, setScore] = useState(0);
+	const [cards, setCards] = useState(cardList);
 
-  const shuffleCards = (cardCopy) => {
-    for (let i = 0; i < cardCopy.length; i++) {
-      let j = Math.floor(Math.random() * (i + 1));
-      [cardCopy[i], cardCopy[j]] = [cardCopy[j], cardCopy[i]];
-    }
-    setCards(cardCopy);
-  };
+	const shuffleCards = () => {
+		const cardsCopy = [...cards];
+		for (let i = 0; i < cardsCopy.length; i++) {
+			let j = Math.floor(Math.random() * (i + 1));
+			[cardsCopy[i], cardsCopy[j]] = [cardsCopy[j], cardsCopy[i]];
+		}
+		setCards(cardsCopy);
+	};
 
-  const toggleClicked = (passedCard, cardsCopy) => {
-    setCards(
-      cardsCopy.map((card) => {
-        if (card.id === passedCard.id) {
-          return {
-            ...card,
-            clicked: true,
-          };
-        } else return card;
-      }),
-    );
-    setScore(score + 1);
-  };
+	const toggleClicked = (passedCard) => {
+		setCards((c) =>
+			c.map((card) => {
+				if (card.id === passedCard.id) {
+					return {
+						...card,
+						clicked: true,
+					};
+				} else return card;
+			}),
+		);
+		setScore(score + 1);
+	};
 
-  const resetGame = (cardsCopy) => {
-    setCards(
-      cardsCopy.map((card) => {
-        return {
-          ...card,
-          clicked: false,
-        };
-      }),
-    );
-    setScore(0);
-  };
+	const resetGame = () => {
+		setCards((c) =>
+			c.map((card) => {
+				return {
+					...card,
+					clicked: false,
+				};
+			}),
+		);
+		setScore(0);
+	};
 
-  const handleClick = (card) => {
-    const cardsCopy = [...cards];
-    shuffleCards(cardsCopy);
-    card.clicked ? resetGame(cardsCopy) : toggleClicked(card, cardsCopy);
-  };
+	const handleClick = (card) => {
+		shuffleCards();
+		card.clicked ? resetGame() : toggleClicked(card);
+	};
 
-  if (score > bestScore) {
-    bestScore = score;
-  }
+	let bestScore = 0;
 
-  return (
-    <>
-			<Header score={score} bestScore={bestScore}/>
-			<Content click={handleClick} cards={cards}/>
-			<Footer/>
+	// Compute the bestscore on render
+	if (score > bestScore) {
+		bestScore = score;
+	}
+
+	return (
+		<>
+			<Header score={score} bestScore={bestScore} />
+			<Content click={handleClick} cards={cards} />
+			<Footer />
 		</>
-  );
+	);
 }
 
 export default App;
