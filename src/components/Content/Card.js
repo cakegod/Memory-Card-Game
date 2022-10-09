57970f1ea128bce4b21d8d071f9b77d39fb3d265
cakegod <cakemancerr@gmail.com>
2022-10-09 12:34:29 +0000

    Update Card.js

--- src/components/Content/Card.js
+++ src/components/Content/Card.js
@@ -1,4 +1,4 @@
-import React, { memo, useEffect } from "react";
+import React, { memo } from "react";
 import { useCardsStore, useScoreStore } from "../../store";
 import { motion } from "framer-motion";
 
